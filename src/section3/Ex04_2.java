3d31fbfeecbfbf002632d0de800ce35b003f3557
JunBe <wnsqowid0901@gmail.com>
2024-05-11 15:20:52 +0900

    연속 부분수열 다른풀이

---
+++ src/section3/Ex04_2.java
@@ -0,0 +1,41 @@
+package section3;
+
+import java.util.Scanner;
+//연속 부분수열 다른 풀이
+
+public class Ex04_2 {
+    public static int solution(int N, int M, int[] arr) {
+        int ans = 0;
+        int lt = 0, rt = 0;
+        int sum = 0;
+        while (rt < N) {
+            if (sum == M) { //값을 찾았을 때 ans카운트 후 lt 한칸 올리며 lt값을 빼준다
+                ans++;
+                sum -= arr[lt++];
+            } else if (sum > M) {  //M 초과했을 경우 -> lt 한칸 올려주며 그 전 lt값 빼주기
+                sum -= arr[lt++];
+            } else { //M보다 작을 때 -> 현재 arr[rt]값을 더한 후 rt카운트
+                sum += arr[rt++];
+                if (sum == M && rt == N) { //rt가 마지막 인덱스에 도착했을 때 sum과 M이 같다면
+                                           //while문이 바로 끝나므로 ans 카운트 해주고 종료
+                    ans++;
+                }
+            }
+
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(solution(N, M, arr));
+    }
+}
