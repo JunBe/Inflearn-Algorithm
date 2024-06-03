d81b97de1886ce07c953c218d44be3459a1dca2e
JunBe <wnsqowid0901@gmail.com>
2024-06-03 15:07:32 +0900

    백트래킹 - 냅색

--- src/other/Assignment.java
+++ src/other/Assignment.java
@@ -45,14 +45,14 @@ public class Assignment {
     }
 
     public static void knapsack(int i, int profit, int weight,int W,int[] profits,int[] weights) {
-        if (weight <= W && profit > maxProfit) {
+        if (weight <= W && profit > maxProfit) { //profit이 가장 좋다면 bestset에 저장
             maxProfit = profit;
             numBest = i;
             bestSet = include.clone();
         }
 
-        if (promising(i, profit,weight, W, profits, weights)) {
-            if (i < profits.length) {
+        if (promising(i, profit, weight, W, profits, weights)) {
+            if (i < profits.length) { //1
                 include[i] = "yes";
                 knapsack(i + 1, profit + profits[i], weight + weights[i], W, profits, weights);
                 include[i] = "no";
@@ -64,25 +64,26 @@ public class Assignment {
     public static boolean promising(int i,int profit, int weight,int W,int[] p,int[] w) {
         int j, k;
         int totWeight; // 현재까지의 무게
-        float bound;
-        int n = w.length;
-        if (weight >= W) {
+        float bound; // 한계
+        int n = w.length; // 아이템의 개수
+        if (weight >= W) { // 최대 무게를 넘으면 promising 하지 않음
             return false;
-        } else {
+        } else { // 최대 무게를 넘지 않을 경우
             j = i;
             bound = profit;
             totWeight = weight;
 
-            while (j < n && totWeight + w[j] <= W) {
-                totWeight += w[j];
-                bound += p[j];
+            while (j < n && totWeight + w[j] <= W) { // 아이템의 끝까지 돌며 무게가 최대 무게보다 작거나 같을 때 까지 반복
+                totWeight += w[j]; // 현재 무게에 아이템[j]의 무게를 더해준다
+                bound += p[j]; // 한계에 아이템[j] 의 이익을 더해준다
                 j++;
             }
-            k = j;
-            if (k < n) {
+            k = j; //2
+            if (k < n) { //만약 j가 아이템의 끝까지 안 돌았다면
+                // 남은 무게를 넣을 수 있는 만큼 쪼개서 한계에 집어넣기
                 bound += (W - totWeight) * p[k] / (float) w[k];
             }
-            return bound > maxProfit;
+            return bound > maxProfit; // 지금까지 나왔던 maxProfit보다 bound가 크다면 promising하므로 true 반환
         }
     }
 }
