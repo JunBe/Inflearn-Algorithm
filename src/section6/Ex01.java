5f2ce96ffa02d745b25f23d16204c88405a6007b
JunBe <wnsqowid0901@gmail.com>
2024-08-22 00:42:43 +0900

    선택 정렬

--- src/section6/Ex01.java
+++ src/section6/Ex01.java
@@ -1,8 +1,34 @@
 package section6;
 
+import java.util.Scanner;
+
 //선택 정렬
 public class Ex01 {
-    public static void main(String[] args) {
 
+    public static void solution(int N, int[] arr) {
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (arr[i] > arr[j]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+
+        //정답
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int arr[] = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        solution(N,arr);
     }
 }
