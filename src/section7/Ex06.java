79c0c57054fa65e769208f373c846b3a3913c67e
JunBe <wnsqowid0901@gmail.com>
2024-09-02 23:56:37 +0900

    집합 구하기

--- src/section7/Ex06.java
+++ src/section7/Ex06.java
@@ -2,27 +2,41 @@ package section7;
 
 import java.util.Scanner;
 //집합 구하기
-class Node {
-    public int data;
-    public Node lt, rt;
-
-    public Node(int data) {
-        this.data = data;
-        this.lt = null;
-        this.rt = null;
-    }
-}
+//class Node {
+//    public int data;
+//    public Node lt, rt;
+//
+//    public Node(int data) {
+//        this.data = data;
+//        this.lt = null;
+//        this.rt = null;
+//    }
+//}
 
 public class Ex06 {
+    static int[] ch;
+    static int N;
 
-    public static int DFS(int n,Node root) {
-
+    public static void DFS(int L) {
+        if (L == N + 1) {
+            for (int i = 1; i < N + 1; i++) {
+                if (ch[i] == 1) {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+        } else {
+            ch[L] = 1;
+            DFS(L + 1);
+            ch[L] = 0;
+            DFS(L + 1);
+        }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        Node node = new Node(1);
-        DFS(N,node);
+        N = sc.nextInt();
+        ch = new int[N + 1];
+        DFS(1);
     }
 }
