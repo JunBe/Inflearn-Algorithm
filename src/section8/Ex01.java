d87651e380d4f092ffc6da7f10b326a054cea25f
JunBe <wnsqowid0901@gmail.com>
2024-09-11 23:30:06 +0900

    합이 같은 부분집합

--- src/section8/Ex01.java
+++ src/section8/Ex01.java
@@ -4,18 +4,41 @@ import java.util.Scanner;
 
 //합이 같은 부분집합
 public class Ex01 {
-    public static void DFS(int[] arr) {
+    static int[] arr;
+    static int arrSum = 0;
+    static String ans = "NO";
+    public static String DFS(int idx,int sum) {
+        if (idx == 0) {
+            if (arrSum % 2 == 1) {
+                return ans = "NO";
+            } else {
+                arrSum /= 2;
+            }
+        }
+        int len = arr.length;
 
+        if (idx == len) {
+            return "";
+        } else {
+            DFS(idx + 1, sum);
+            sum += arr[idx];
+            if (sum == arrSum) {
+                ans= "YES";
+            }
+            DFS(idx + 1, sum);
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] arr = new int[n];
+        arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
+            arrSum += arr[i];
         }
+        System.out.println(DFS(0, 0));
 
-        DFS(arr);
     }
 }
