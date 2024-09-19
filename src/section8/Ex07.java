5acd85f05c7149f0baac6865caaa01ee20b241b0
JunBe <wnsqowid0901@gmail.com>
2024-09-20 01:29:22 +0900

    조합

--- src/section8/Ex07.java
+++ src/section8/Ex07.java
@@ -3,19 +3,25 @@ package section8;
 import java.util.Scanner;
 
 public class Ex07 {
-    private static void solution() {
-        //5 3 -> 10
-        //5C3 = 4C2 + 4C3 = 6 + 4 = 10
-        //4C2 = 3C1 + 3C2 = 3 + 3 = 6  / 4C3 = 3C2 + 3C3 = 3 + 1 = 4
-        //3C1 = 2C0 + 2C1 = 1 + 2 = 3 / 3C2 = 2C1 + 2C2 = 2 + 1 = 3 /  3C3 = 1
-        //2C1 = 1C0 + 1C1 = 1 + 1 = 2
+    static int sum = 0;
+    private static void DFS(int n, int r) {
+        if (r == 0 || r == n) {
+            sum++;
+            return;
+        } else {
+            DFS(n - 1, r - 1);
+            DFS(n - 1, r);
+        }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int r = sc.nextInt();
-
-        solution();
+        if (n / r < 2) {
+            r = n - r;
+        }
+        DFS(n, r);
+        System.out.println(sum);
     }
 }
