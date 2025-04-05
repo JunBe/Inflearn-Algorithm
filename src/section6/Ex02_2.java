c09feb5e9b49a3d307c8b0fa175056360119db95
jeonjunbae <wnsqowid0901@gmail.com>
2025-04-05 20:43:38 +0900

    버블 정렬

---
+++ src/section6/Ex02_2.java
@@ -0,0 +1,33 @@
+package section6;
+
+import java.util.Scanner;
+
+// 버블 정렬
+public class Ex02_2 {
+    public static int[] solution(int N, int[] arr) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int val : solution(N, arr)) {
+            System.out.print(val + " ");
+        }
+    }
+}
